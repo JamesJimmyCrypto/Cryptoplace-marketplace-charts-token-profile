9f6d302aa4a2ddc4be33a4aa64c231eda9fcc7f7
Mustafa Memon <mustafamemon360@gmail.com>
2024-06-11 03:44:59 +0500

    Successfully Completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,48 +3,51 @@ import './Navbar.css';
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
-  const {setCurrency} = useContext(CoinContext);
-  
+  const { setCurrency } = useContext(CoinContext);
+
   const currencyHandler = (event) => {
-    switch(event.target.value){
-      case "usd" : {
-        setCurrency({name: "usd", symbol: "$"});
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
         break;
       }
-      case "eur" : {
-        setCurrency({name: "eur", symbol: "€"});
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
         break;
       }
-      case "pkr" : {
-        setCurrency({name: "pkr", symbol: "Rs"});
+      case "pkr": {
+        setCurrency({ name: "pkr", symbol: "Rs" });
         break;
-        }
-          case "inr" : {
-            setCurrency({name: "inr", symbol: "₹"});
-            break;
-          }
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
       default: {
-        setCurrency({name: "usd", symbol: "$"});
+        setCurrency({ name: "usd", symbol: "$" });
         break;
       }
     }
   }
   return (
     <div className='navbar'>
-      <img src={logo} alt="logo" className='logo'/>
+      <Link to={'/'}>
+        <img src={logo} alt="logo" className='logo' />
+      </Link>
       <ul>
-        <li>Home</li>
+        <Link to={'/'}><li>Home</li></Link>
         <li>Features</li>
         <li>Pricing</li>
         <li>Blog</li>
       </ul>
       <div className="nav-right">
         <select onChange={currencyHandler}>
-            <option value="usd">USD</option>
-            <option value="eur">EUR</option>
-            <option value="inr">INR</option>
-            <option value="pkr">PKR</option>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
+          <option value="pkr">PKR</option>
         </select>
         <button>Sign up <img src={arrow_icon} alt="arrow-icon" /> </button>
       </div>
